d7f43282861d35770a98ede62747a12aa95b0890
eddiexue <jxuedi@vip.qq.com>
2017-03-02 12:52:54 +0800

    add main

--- httpsvrmain.go
+++ httpsvrmain.go
@@ -3,36 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"webrtcmcu/signaling"
-
 	"os"
-
-	_ "github.com/gorilla/websocket"
 )
 
 func main() {
 	log.Println("Starting serivces...")
 
-	//创建websocket服务
-	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
-		signaling.ServeWebSocket(w, r)
-	})
-
 	//创建https文件服务
-	http.Handle("/", http.FileServer(http.Dir("browser")))
+	http.Handle("/", http.FileServer(http.Dir(".")))
 
 	//// One can use generate_cert.go in crypto/tls to generate cert.pem and key.pem.
-	err := http.ListenAndServeTLS(":8888", "signaling/cert/certificate.pem", "signaling/cert/privatekey.pem", nil)
+	//err := http.ListenAndServeTLS(":8888", "signaling/cert/certificate.pem", "signaling/cert/privatekey.pem", nil)
+	err := http.ListenAndServe(":8888", nil)
+
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 	//代码不会执行到这里来！！
-}
-
-//暂时没用先放着
-func httpRequestHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("Nothing for now.\n"))
 }
