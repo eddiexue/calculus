0270c38a62d9bcd0b0b19fc03b7c7b8a03ed2842
eddiexue <jxuedi@vip.qq.com>
2017-03-10 23:22:22 +0800

    new test

---
+++ tgpl/cpt7/chapter7.go
@@ -0,0 +1,44 @@
+package cpt7
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"time"
+)
+
+type Point struct{ X, Y float64 }
+
+// same thing, but as a method of the Point type
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+type MyWriter struct {
+	buflen int
+}
+
+func (mw *MyWriter) Write(p []byte) (n int, err error) {
+	return len(p), nil
+}
+
+func test() {
+	var w io.Writer
+	w = os.Stdout         // OK: *os.File has Write method
+	w = new(bytes.Buffer) // OK: *bytes.Buffer has Write method
+	_ = w
+
+	mw := &MyWriter{buflen: 100}
+	w = mw
+	//---------------------------
+
+	var period = flag.Duration("period", 3*time.Second, "sleep period")
+
+	flag.Parse()
+	fmt.Println("Sleeping for %v", *period)
+	time.Sleep(*period)
+	fmt.Println()
+}
