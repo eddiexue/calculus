62be89058103b0827891e9d39836a1154f809357
eddiexue <jxuedi@vip.qq.com>
2017-06-14 10:14:47 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# calculus
+# 设定含有两个目录的GoPath（UNIX）
 
 GOPATH=/Users/eddiexue/Tencent/Projects/gotools:/Users/eddiexue/Tencent/Projects/goworks
 export GOPATH
 
 $ 特殊写法，为了支持GOPATH中配置多个目录
 PATH=${GOPATH//://bin:}/bin:$PATH
+
+# Googe Chart离线使用方法
+
+## 大原则
+在能访问Google的网络下运行，如果遇到图形加载失败，就到Chrome控制台看看哪个js下载404了，下载到本地`/gchartlib`下相应目录即可
+
+## loader.js
+`https://www.gstatic.com/charts`，替换成本地路径`/gchartlib`。注意这里定义了GoogleChart的版本号~~
+
+## version/loader.js
+`https://www.gstatic.com/charts`替换成本地路径`/gchartlib`
--- httpsvrmain.go
+++ httpsvrmain.go
@@ -1,27 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"calculus/charts"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 )
 
 func ServeExample(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	if r.URL.Path != "/mline" && r.URL.Path != "/mlinedaul" {
-		http.Error(w, "Not found", 404)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", 405)
-		return
-	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	if r.URL.Path == "/mline" {
-		tpl, err := template.ParseFiles("tpls/egmline.html")
+	if r.URL.Path == "/eg/mline" {
+		tpl, err := template.ParseFiles("tpls/eg/egmline.html")
 		if err != nil {
 			http.Error(w, err.Error(), 500)
 			return
@@ -30,8 +23,8 @@ func ServeExample(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if r.URL.Path == "/mlinedaul" {
-		tpl, err := template.ParseFiles("tpls/egmlinedaul.html")
+	if r.URL.Path == "/eg/mlinedaul" {
+		tpl, err := template.ParseFiles("tpls/eg/egmlinedaul.html")
 		if err != nil {
 			http.Error(w, err.Error(), 500)
 			return
@@ -39,6 +32,43 @@ func ServeExample(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, r.Host)
 		return
 	}
+
+	if r.URL.Path == "/eg/pie" {
+		tpl, err := template.ParseFiles("tpls/eg/egpie.html")
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		//dataT的内容可以自动生成
+		dataT := []struct {
+			key   string
+			value int
+		}{
+			{"Mushrooms", 3},
+			{"Onions", 1},
+			{"Olives", 1},
+			{"Zucchini", 1},
+			{"Pepperoni", 2},
+		}
+
+		//用dataT生成js所需要的字符串
+		var strBuf bytes.Buffer
+		for i := 0; i < len(dataT); i++ {
+			strBuf.WriteString("[")
+			strBuf.WriteString(fmt.Sprintf("'%s', %d", dataT[i].key, dataT[i].value))
+			strBuf.WriteString("]")
+			if i != len(dataT)-1 {
+				strBuf.WriteString(",\n")
+			}
+		}
+
+		//JS里面有个DataRow字段，该字段类型必须是template.JS！
+		tpl.Execute(w, struct{ DataRows template.JS }{DataRows: template.JS(strBuf.String())})
+		return
+	}
+
+	http.Error(w, "Not found", 404)
 }
 
 func main() {
@@ -46,14 +76,12 @@ func main() {
 
 	//创建https文件服务
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.HandleFunc("/mline", ServeExample)
-	http.HandleFunc("/mlinedaul", ServeExample)
-	http.HandleFunc("/charts/", charts.ServeChart)
-	err := http.ListenAndServe(":9527", nil)
+	http.HandleFunc("/eg/", ServeExample)
+	http.HandleFunc("/ch/", charts.ServeChart)
 
+	err := http.ListenAndServe(":9527", nil)
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 	//代码不会执行到这里来！！
 }
